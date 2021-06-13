3bf26a592116b7784cc3913ade56bed4464925d4
kazumasa.nakamura <kazumasa.nakamura@vitalize.co.jp>
2021-06-13 15:37:38 +0900

    display todo list by streambuilder

--- lib/Pages/todo_list_page.dart
+++ lib/Pages/todo_list_page.dart
@@ -3,14 +3,29 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 
 class TodoListPage extends StatelessWidget {
 
+  Widget _buildList(QuerySnapshot snapshot) {
+    return ListView.builder(
+      itemCount: snapshot.docs.length,
+      itemBuilder: (context, index) {
+        final doc = snapshot.docs[index];
+        // QueryDocumentSnapshot
+        // QuerySnapshotが持っているデータの中身一つ一つを持っています
+        final Map map = doc.data();
+        return ListTile(
+            title: Text(map['title'])
+        );
+      }
+    );
+  }
+
+
   Widget _buildBody(BuildContext context) {
 
     final TextEditingController _controller = TextEditingController();
 
     void _addTask() {
 
-      FirebaseFirestore.instance.collection('todos')
-          .add({"title": _controller.text});
+      FirebaseFirestore.instance.collection('todos').add({"title": _controller.text});
       _controller.text = "";
 
     }
@@ -34,18 +49,26 @@ class TodoListPage extends StatelessWidget {
               )
             ],
           ),
-        
-        
-        Expanded(
-          child: ListView.builder(
-            itemCount: 20,
-            itemBuilder: (context, index) {
-              return ListTile(
-                title: Text("Your task here..."),
-              );
-            }
-          ),
+        // イベントが起きるたびにその情報を自動でキャッチして、アプリの画面に表示を反映
+        // これらのイベントをstreamと呼ぶ。
+        // streamから流れてくるイベントを監視して、
+        // 新しいイベントが発生する度に、再ビルドでき常に最新の状態を表示できる
+        // Querysnapshotとは複数のドキュメントデータを持っている。
+        // データベースから複数のドキュメントを取得した時はquerysnapshotがこの形でデータを持っている。
+        StreamBuilder<QuerySnapshot>(
+          // todosに変更が会ったときに、そのデータをstreamに流す。
+          stream: FirebaseFirestore.instance.collection("todos").snapshots(),
+          // snapshot of stream snapshot are consist of these documents
+          builder: (context, snapshot) {
+            // ロード中
+            if (!snapshot.hasData) return LinearProgressIndicator();
+
+            return Expanded(
+              child: _buildList(snapshot.data)
+            );
+          }
         )
+       
         ],
       ),
     );
